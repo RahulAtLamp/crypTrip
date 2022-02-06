5f04987e77b7b87ae0471036abdb8550b7575c52
RahulAtLamp <rahulrajan@lampros.tech>
2022-02-06 19:26:33 +0530

    LTS code 6 Feb 2022 07:26

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,36 @@
 import '../styles/globals.scss'
 import Link from 'next/link'
 import Image from 'next/image'
-import Logo from '../public/svgs/logo'
+import { useRouter } from 'next/router'
+import { Web3Modal } from "web3modal"
+
+import { ContractAddress } from '../config'
+
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  async function onInit() {
+    if(typeof web3 !== 'undefined'){
+      await window.ethereum.enable();
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = accounts[0];
+      // console.log(typeof account)
+      // console.log(account)
+      window.ethereum.on('accountsChanged', function (accounts) {
+        // Time to reload your interface with accounts[0]!
+        // console.log(accounts[0])
+        return accounts[0]
+      });
+      setAcc(account);
+    }
+    else{
+      return null;
+    }
+  }
+
+
   return (
     <div>
       <nav className='navs'>
@@ -15,13 +42,13 @@ function MyApp({ Component, pageProps }) {
         </div>
         <div className='nav-right'>
           <div className='nav-links'>
-            <div className='nav-link'>
+            <div className='nav-link' style={ (router.pathname) === "/" ?   { color:"#f29100", fontWeight:'700' } : null }>
               <Link href="/">Home</Link>
             </div>
-            <div className='nav-link'>
+            <div className='nav-link' style={ (router.pathname) === "/booking" ?   { color:"#f29100", fontWeight:'700' } : null }>
               <Link href="/booking">Book</Link>
             </div>
-            <div className='nav-link'>
+            <div className='nav-link' style={ (router.pathname) === "/standings" ?   { color:"#f29100", fontWeight:'700' } : null }>
               <Link href="/standings">Standing</Link>
             </div>
             <div className='nav-dashboard'>
@@ -30,13 +57,13 @@ function MyApp({ Component, pageProps }) {
                   Dashborad
                 </div>
                 <div className='nav-hover-list' >
-                <div className='nav-link sub'>
+                <div className='nav-link sub' style={ (router.pathname) === "/user-info" ?   { color:"#f29100", fontWeight:'700' } : null }>
                   <Link href="/user-info">User Info</Link>
                 </div>
-                <div className='nav-link sub'>
+                <div className='nav-link sub' style={ (router.pathname) === "/collection" ?   { color:"#f29100", fontWeight:'700' } : null }>
                   <Link href="/collection">My Collection</Link>
                 </div>
-                <div className='nav-link sub'>
+                <div className='nav-link sub' style={ (router.pathname) === "/create" ?   { color:"#f29100", fontWeight:'700' } : null }>
                   <Link href="/create">Create NFT</Link>
                 </div>
               </div>
